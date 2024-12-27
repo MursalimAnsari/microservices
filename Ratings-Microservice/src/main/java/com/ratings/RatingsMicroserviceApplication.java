512ddb178c8ca3ec2cd4696f0ed8105415376f73
MursalimAnsari <mursalim.ansari.1998@gmail.com>
2024-12-27 18:01:03 +0530

    added api gateway and feign client

---
+++ Ratings-Microservice/src/main/java/com/ratings/RatingsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ratings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsMicroserviceApplication.class, args);
+	}
+
+}
